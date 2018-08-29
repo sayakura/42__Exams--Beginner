5db79bd45f7283799573b7ec38f340b49dac69d6
Kura Peng <kpeng@e1z1r3p24.42.us.org>
2018-08-29 04:58:32 -0700

    ,

--- day06/ex00/libft_creator.sh
+++ day06/ex00/libft_creator.sh
@@ -1,7 +1,4 @@
-gcc -c ft_putchar.c -o ft_putchar.o
-gcc -c ft_swap.c -o ft_swap.o
-gcc -c ft_putstr.c -o ft_putstr.o
-gcc -c ft_strlen.c -o ft_strlen.o
-gcc -c ft_strcmp.c -o ft_strcmp.o
-
-ar -rv libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+#!/bin/sh
+gcc -c *.c 
+ar rc libft.a *.o
+rm *.o
