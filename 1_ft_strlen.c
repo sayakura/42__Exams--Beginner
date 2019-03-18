f4a4f0c0d55938af3d5221eee2430e1a859ff1be
sayakura <linra.kura@gmail.com>
2019-03-18 06:21:01 -0700

    fast strlen

--- 1_ft_strlen.c
+++ 1_ft_strlen.c
@@ -3,6 +3,17 @@
  * Created : 2018/10/05
  * Updated : 2018/10/05
  * ***************************************************************************/
+unsigned int ft_strlen(const char *s) {
+    unsigned int len = 0;
+    while(1) {
+        unsigned x = *(unsigned*)s;
+        if((x & 0xFF) == 0) return len;
+        if((x & 0xFF00) == 0) return len + 1;
+        if((x & 0xFF0000) == 0) return len + 2;
+        if((x & 0xFF000000) == 0) return len + 3;
+        s += 4, len += 4;
+    }
+}
 
 int	ft_strlen(char *str)
 {
