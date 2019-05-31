1cfacdff6f2ce72a568fba751a76c13aedc23e65
Kura Peng <linra.kura@gmail.com>
2019-05-30 19:12:28 -0700

    Update 4_flood_fill.c

--- 4_flood_fill.c
+++ 4_flood_fill.c
@@ -4,44 +4,28 @@
  * Updated : 2018/10/05
  * ***************************************************************************/
 
-typedef struct 	s_point {
-    int 		x;				// x : Width  | x-axis
-    int 		y;				// y : Height | y-axis
-} 				t_point;
+#include "flood_fill.h"
+
+void f_fill(char **tab, t_point size, char target, int row, int col)
+{
+    if (row < 0 || col < 0 || row >= size.y || col >= size.x)
+        return;
+    if (tab[row][col] == 'F' || tab[row][col] != target)
+        return;
+    tab[row][col] = 'F';
+
+    f_fill(tab, size, target, row -1, col);
+    f_fill(tab, size, target, row +1, col);
+    f_fill(tab, size, target, row, col - 1);
+    f_fill(tab, size, target, row, col + 1);
+}
 
 void  flood_fill(char **tab, t_point size, t_point begin)
 {
-	t_point temp;
-	
-	if (tab[begin.x][begin.y] == '0')
-	{
-		tab[begin.x][begin.y] = 'F';
-		if (tab[begin.x][begin.y + 1] == '0')
-		{
-			temp.x = begin.x;
-			temp.y = begin.y + 1;
-			flood_fill(tab, size, temp);
-		}
-		if (tab[begin.x + 1][begin.y] == '0')
-		{
-			temp.x = begin.x + 1;
-			temp.y = begin.y;
-			flood_fill(tab, size, temp);
-		}
-		if (tab[begin.x - 1][begin.y] == '0')
-		{
-			temp.x = begin.x - 1;
-			temp.y = begin.y;
-			flood_fill(tab, size, temp);
-		}
-		if (tab[begin.x][begin.y - 1] == '0')
-		{
-			temp.x = begin.x;
-			temp.y = begin.y - 1;
-			flood_fill(tab, size, temp);
-		}
-	}
+    char target = tab[begin.y][begin.x];
+    f_fill(tab, size, target, begin.y, begin.x);
 }
+
 /*------------------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
